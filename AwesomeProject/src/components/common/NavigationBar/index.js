f1bc609e2624efa88071a8c920ff2ee0c08cf930
LiYanan07 <yanan.li07@ele.me>
2018-12-26 17:37:45 +0800

    navigatorbar init

---
+++ AwesomeProject/src/components/common/NavigationBar/index.js
@@ -0,0 +1,27 @@
+
+import React, {Component} from 'react'
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native'
+import styles from './styles'
+
+const NAVBAR_HEIGHT_IOS = 44
+CONST NAVBAR_HEIGHT_ANDROID = 50
+
+export default class NavigationBar extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+
+    }
+  }
+
+  render () {
+    return(
+      <View style={}></View>
+    )
+  }
+}
---
+++ AwesomeProject/src/components/common/NavigationBar/styles.js
@@ -0,0 +1,11 @@
+export default {
+  page: {
+    flex: 1
+  },
+  status: {
+    backgroundColor: '#008BBF'
+  },
+  webview: {
+    flex: 1
+  }
+}
